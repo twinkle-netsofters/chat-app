1c848bfcd4fbb3f5f048d9c5ff7be9879fd22420
Dhwaneel9913 <dhwaneel9913@gmail.com>
2023-02-16 16:19:59 +0530

    login screen auth changes

--- lib/Screens/auth/loginScreen.dart
+++ lib/Screens/auth/loginScreen.dart
@@ -1,4 +1,7 @@
+import 'package:chat_app/Models/userModel.dart';
 import 'package:chat_app/Screens/auth/signupScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,59 +12,112 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
+
+  void checkValues() {
+    String email = emailController.text.trim();
+    String password = pwdController.text.trim();
+// for validation
+    if (email == "" || password == "") {
+      print('Please fill both fields');
+    } else {
+      logIn(email, password);
+    }
+  }
+
+  void logIn(String email, String password) async {
+    UserCredential? credential;
+//sign in dataaa
+    try {
+      credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (ex) {
+      print('error ${ex.code}');
+    }
+    //
+    if (credential != null) {
+      String? uid = credential.user!.uid;
+      //A DocumentSnapshot contains data read from a document in your FirebaseFirestore database.
+      // The data can be extracted with the data property or by using subscript syntax to access a specific field.
+      DocumentSnapshot userData =
+          await FirebaseFirestore.instance.collection("users").doc(uid).get();
+      // fromJSon method converts the json data in object usin fromJson
+      // here data is not in map format for now so we have to convert that to map using .data() as MAp<>
+      UserModel? userModel =
+          UserModel.fromJson(userData.data() as Map<String, dynamic>);
+    }
+
+    print('login successfullll');
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: height /6,
-                ),
-                Text('CHAT APP',style: TextStyle(color: ThemeData.light().primaryColor,fontSize: 40,fontWeight: FontWeight.bold),),
-                SizedBox(
-                  height: height /6,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                   // hintText: 'Please add your email',
-                    label: Text('Email'),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: height / 6,
                   ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    label: Text('Password'),
+                  Text(
+                    'CHAT APP',
+                    style: TextStyle(
+                        color: ThemeData.light().primaryColor,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(
-                  height: height /9,
-                ),
-                Container(
-                    width: width/2,
-                    child: ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
-                    }, child: Text('Login')))
-              ],
+                  SizedBox(
+                    height: height / 6,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      // hintText: 'Please add your email',
+                      label: Text('Email'),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: pwdController,
+                    decoration: InputDecoration(
+                      label: Text('Password'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height / 9,
+                  ),
+                  Container(
+                      width: width / 2,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            checkValues();
+                          },
+                          child: Text('Login')))
+                ],
+              ),
             ),
           ),
         ),
-      ),
         bottomNavigationBar: Container(
-          child:  Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('Dont have an account ?'),
-              TextButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
-              }, child: Text('Sign up'))
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SignUpScreen()));
+                  },
+                  child: Text('Sign up'))
             ],
           ),
-        )
-    );
+        ));
   }
 }
--- lib/Screens/auth/signupScreen.dart
+++ lib/Screens/auth/signupScreen.dart
@@ -18,9 +18,11 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController confrimPwdController = TextEditingController();
 
   void checkValues() {
+    // trims the spaces and saves it to a variable
     String email = emailController.text.trim();
     String pwd = pwdController.text.trim();
     String cPwd = confrimPwdController.text.trim();
+    // for validatiom
     if (email == "" || pwd == "" || cPwd == "") {
       print('Please fill all the values');
     } else if (pwd != cPwd) {
@@ -31,23 +33,29 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   void signUp(String email, String pwd) async {
+    // creates object for credentials
     UserCredential? credential;
-
     try {
+      //here credentials - email , pwd are adeed to the credentials object
       credential = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: pwd);
     } on FirebaseAuthException catch (e) {
+      // if error comes
       print(e.message.toString());
     }
-
+// if there is not error while saving to the authentication in firebase
     if (credential != null) {
+      // uid given by firebse
       String uid = credential.user!.uid;
+      //saves the value to firebase using userModel newUser Obejct
       UserModel newUser = UserModel(
         uid: uid,
         email: email,
         first_name: "",
         profile_pic: "",
       );
+
+      //  nowww first its creating a collection nemed "users" then its creates documetn and then converts data to the mapa and then in json then message retrive "user created"
       await FirebaseFirestore.instance
           .collection("users")
           .doc(uid)
